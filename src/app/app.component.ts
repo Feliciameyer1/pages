effbc6c15029413782b45f9a640b85f33216a13e
felicia Meyer <feliciameyer1@student.purdueglobal.edu>
2021-07-16 10:26:40 -0400

    adding pagination to Pages app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images =[
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'At the Mountains',
+      url: 'https://images.unsplash.com/photo-1486072889922-9aea1fc0a34d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW91dGFpbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Cave Entrance',
+      url: 'https://images.unsplash.com/photo-1589072894364-cdafe42024b2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2F2ZSUyMGVudHJhbmNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'My dog',
+      url: 'https://images.unsplash.com/photo-1552053831-71594a27632d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZG9nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'My Cat',
+      url: 'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2F0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    }
+  ]
 }
