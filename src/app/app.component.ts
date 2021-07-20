80c169d0f61590c391ac817905b9f244b2c95032
felicia Meyer <feliciameyer1@student.purdueglobal.edu>
2021-07-20 12:53:55 -0400

    adjusted the pagination for longer lists of images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,4 +29,7 @@ export class AppComponent {
       url: 'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2F0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
     }
   ]
+  checkWindowIndex(index:number){
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
